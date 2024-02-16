c04d74f9dbf7bc3e04d6aab2ac66ea2e28b0431b
MaryiaPavireyeva <povireevamaryia@gmail.com>
2024-02-16 14:18:16 +0300

    new

--- src/HW2.java
+++ src/HW2.java
@@ -1,7 +1,7 @@
 public class HW2 {
     public static void main(String[] args) {
-        int x = 1;
-        int y = 9;
+//        int x = 1;
+//        int y = 9;
         //#1
 //        if (x > y) {
 //            System.out.println(x);
@@ -13,11 +13,65 @@ public class HW2 {
 //            } else
 //            System.out.println(y);
         //#3
-        if (x == 1) {
-            System.out.println("A");
-        } else if (x == 2) {
-            System.out.println("B");
-        } else
-            System.out.println("C");
+//        if (x == 1) {
+//            System.out.println("A");
+//        } else if (x == 2) {
+//            System.out.println("B");
+//        } else
+//            System.out.println("C");
+        //#4
+//        if (x == 1) {
+//            System.out.println("A");
+//        } else if (x == 2) {
+//            System.out.println("B");
+//        } else if (x == 3) {
+//            System.out.println("C");
+//        } else if (x == 4) {
+//            System.out.println("D");
+//        } else
+//            System.out.println();
+        //#5
+//        if (x > 100) {
+//            x -= 10;
+//            System.out.println(x);
+//        } else
+//            System.out.println(x);
+        //#6
+        int time = 1;
+        switch (time) {
+            case 1:
+                System.out.println(13);
+                break;
+            case 2:
+                System.out.println(14);
+                break;
+            case 3:
+                System.out.println(15);
+                break;
+            case 4:
+                System.out.println(16);
+                break;
+            case 5:
+                System.out.println(17);
+                break;
+            case 6:
+                System.out.println(18);
+                break;
+            case 7:
+                System.out.println(19);
+                break;
+            case 8:
+                System.out.println(20);
+                break;
+            case 9:
+                System.out.println(21);
+                break;
+            case 10:
+                System.out.println(22);
+                break;
+            case 11:
+                System.out.println(23);
+                break;
+        }
     }
 }

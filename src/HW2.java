814e3c5a49dee23c15128fe90aad5518e8ed5a28
MaryiaPavireyeva <povireevamaryia@gmail.com>
2024-02-16 11:21:45 +0300

    new updates

--- src/HW2.java
+++ src/HW2.java
@@ -1,6 +1,6 @@
 public class HW2 {
     public static void main(String[] args) {
-        int x = 5;
+        int x = 10;
         int y = 9;
         if (x > y) {
             System.out.println(x + ">" + y);
